499bf8076b4084356138ee2bb109c54201c9e4c8
Navid Boloorian <navidboloorian0@gmail.com>
2023-05-24 11:08:05 -0700

    stripped bloat from boilerplate

---
+++ lib/src/utils/custom_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+abstract class CustomColors {
+  static const Color fitsawRed = Color(0xffe03a3e);
+  static const Color fitsawGreen = Color(0xff61bb46);
+  static const Color fitsawBlue = Color(0xff009ddc);
+  static const Color fitsawPurple = Color(0xff963d97);
+  static const Color fitsawOrange = Color(0xfff5821f);
+  static const Color dmScreenBackground = Color(0xff0d1117);
+  static const Color dmPrimaryBlockBackgrond = Color(0xff161b22);
+  static const Color dmSecondaryBlockBacgkround = Color(0xff32383f);
+  static const Color dmPrimaryText = Color(0xffffffff);
+  static const Color dmSecondaryText = Color(0xff161b22);
+}
---
+++ lib/src/utils/themes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:fitsaw/utils/custom_colors.dart';
+
+class Themes {
+  static ThemeData get dark {
+    return ThemeData(
+      primaryColor: CustomColors.dmScreenBackground,
+    );
+  }
+}
